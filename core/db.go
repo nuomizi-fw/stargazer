be227df78a6e5b692da0f6aee2466b111b0d6fcd
Ns2Kracy <ns2kracy@gmail.com>
2024-08-08 14:52:10 +0800

    feat: db

--- core/db.go
+++ core/db.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"time"
+
+	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -8,6 +11,21 @@ type StargazerDB struct {
 	DB *gorm.DB
 }
 
+func (db StargazerDB) AutoMigrate() {
+}
+
 func NewStargazerDB(config StargazerConfig, logger StargazerLogger) StargazerDB {
-	return StargazerDB{}
+	db, err := gorm.Open(sqlite.Open(config.Database.DBFile), &gorm.Config{
+		Logger: logger.GetGormLogger(),
+	})
+	if err != nil {
+		logger.Error("Failed to connect to database", err)
+	}
+
+	c, _ := db.DB()
+	c.SetMaxIdleConns(10)
+	c.SetMaxOpenConns(1)
+	c.SetConnMaxIdleTime(time.Second * 1000)
+
+	return StargazerDB{DB: db}
 }
