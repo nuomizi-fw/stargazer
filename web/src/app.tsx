6706e01933c67c2c0e993ece7048b429277b3abd
ns2kracy <ns2kracy@gmail.com>
2024-09-04 22:21:06 +0800

    feat: Remove unused styled-system import

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -3,7 +3,6 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
-import { css } from "styled-system";
 
 export default function App() {
   return (
